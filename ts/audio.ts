e8864eb7544425e1fd170d2b1a6340f7190871ca
Matt Deeds <mdeeds@gmail.com>
2021-05-15 11:38:28 -0700

    Beep.

---
+++ ts/audio.ts
@@ -0,0 +1,50 @@
+export class Audio {
+  private audioCtx: AudioContext;
+  private oscillator: OscillatorNode;
+  private gain: GainNode;
+  private constructor(context: AudioContext) {
+    this.audioCtx = context;
+    this.oscillator = this.audioCtx.createOscillator();
+    this.oscillator.type = 'square';
+    this.gain = this.audioCtx.createGain();
+    this.oscillator.connect(this.gain);
+    this.oscillator.start();
+    this.gain.connect(this.audioCtx.destination);
+    this.setNote(60);
+  }
+
+  public static async make(): Promise<Audio> {
+    const ctx = await Audio.getAudioContext();
+    return new Promise((resolve, reject) => {
+      resolve(new Audio(ctx));
+    })
+  }
+
+  private static getAudioContext(): Promise<AudioContext> {
+    return new Promise((resolve, reject) => {
+      const context = new window.AudioContext();
+      if (context.state === 'running') {
+        resolve(context);
+      } else {
+        setTimeout(async () => {
+          resolve(await Audio.getAudioContext());
+        }, 500);
+      }
+    });
+  }
+
+  public setNote(note: number) {
+    this.oscillator.frequency.setValueAtTime(
+      440 * Math.pow(2, (note - 69) / 12),
+      this.audioCtx.currentTime);
+  }
+
+  public noteOn() {
+    console.log('On');
+    this.gain.gain.setValueAtTime(1.0, this.audioCtx.currentTime);
+  }
+
+  public noteOff() {
+    this.gain.gain.setValueAtTime(0.0, this.audioCtx.currentTime);
+  }
+}
--- ts/index.ts
+++ ts/index.ts
@@ -1,20 +1,28 @@
+import { Audio } from "./audio";
 import { Render } from "./render";
 
 const body = document.getElementsByTagName('body')[0];
 
-const r = new Render(body);
+async function go() {
+  const r = new Render(body);
+  const a = await Audio.make();
 
-body.addEventListener('keydown', handleKey);
-body.addEventListener('keyup', handleKey);
+  body.addEventListener('keydown', handleKey);
+  body.addEventListener('keyup', handleKey);
 
-function handleKey(ev: KeyboardEvent) {
-  if (ev.type === 'keydown') {
-    if (ev.key === 'Shift') {
-      r.noteOn();
-    }
-  } else if (ev.type === 'keyup') {
-    if (ev.key === 'Shift') {
-      r.noteOff();
+  function handleKey(ev: KeyboardEvent) {
+    if (ev.type === 'keydown') {
+      if (ev.key === 'Shift') {
+        r.noteOn();
+        a.noteOn();
+      }
+    } else if (ev.type === 'keyup') {
+      if (ev.key === 'Shift') {
+        r.noteOff();
+        a.noteOff();
+      }
     }
   }
 }
+
+go();
